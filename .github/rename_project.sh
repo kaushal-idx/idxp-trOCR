674437840fcfe9550ecc1d741df819c93fe9e0a2
kaushal-idx <kaushal-idx@users.noreply.github.com>
2022-02-07 06:29:22 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="kaushal-idx"
+original_name="idxp_trocr"
+original_urlname="idxp-trOCR"
+original_description="Awesome idxp_trocr created by kaushal-idx"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv idxp_trocr $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
